6ba843c6bc304dd289a7dd54263b957d188d92a7
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-05 02:59:54 +0000

    intstall express

--- app.js
+++ app.js
@@ -1,50 +1,33 @@
-//**because readFile and writeFile are promises we can create a new promise to handle errors and data */
-// const { readFile } = require("fs");
-// function getText(path) {
-// 	return new Promise((resolve, reject) => {
-// 		readFile(path, "utf8", (error, data) => {
-// 			if (error) {
-// 				reject(error);
-// 			} else {
-// 				resolve(data);
-// 			}
-// 		});
-// 	});
-// }
+const express = require("express");
+const app = express();
+const path = require("path");
 
-// getText("./content/subfolder/readFile.txt")
-// 	.then((data) => console.log(data))
-// 	.catch((error) => console.log(error));
+app.use(express.static(path.resolve(__dirname, "./public")));
 
-//**OR we could use the .promises on the fs module */
-// const { readFile, writeFile } = require("fs").promises;
-// const start = async () => {
-// 	try {
-// 		const first = await readFile("./content/subfolder/readFile.txt", "utf-8");
-// 		await writeFile(
-// 			"./content/subfolder/newReadFile.txt",
-// 			"I wrote in this file"
-// 		);
-// 		console.log(first);
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
+app.get("/", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./EXPRESS/index.html"));
+});
 
-// start();
+app.get("/about", (req, res) => {
+	res.status(200).send("This is the about page");
+});
 
-//**Or we could use the util module to create a promise */
-// const { readFile } = require("fs");
-// const util = require("util");
-// const promisify = util.promisify(readFile);
+app.get("*", (req, res) => {
+	res.status(404).send("The resource was not found");
+});
 
-// const start = async () => {
-// 	try {
-// 		const first = await promisify("./content/subfolder/readFile.txt", "utf-8");
-// 		console.log(first);
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
+app.listen(5000, () => {
+	console.log("running on port 5000");
+});
 
-// start();
+// const server = app.createServer((req,res)=> {
+
+// })
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
