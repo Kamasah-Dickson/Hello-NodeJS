df083aafd91713eb6e9c338381da0f2f58422e1c
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-05 02:58:00 +0000

    add express

---
+++ EXPRESS/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.listen(5000, () => {
+	console.log("server is running on port 5000");
+});
+
+// const server = app.createServer((req,res)=> {
+
+// })
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
