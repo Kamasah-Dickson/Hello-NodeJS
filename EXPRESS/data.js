1b7c5e53c0d164b63baf9d230f9f7ecd5d8f8d9f
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-06 04:18:18 +0000

    update express data

---
+++ EXPRESS/data.js
@@ -0,0 +1,19 @@
+const people = [
+	{
+		id: 1,
+		name: "Kamasah Dickson",
+		description: "fkdjkfasjfkdjfdioerfijfdojckfifi",
+	},
+	{
+		id: 2,
+		name: "Elizabeth Kamasah",
+		description: "dfjksdjksdjfkjfksjfkdjfkjkjkjkjkjk",
+	},
+	{
+		id: 3,
+		name: "Samuel Kamasah",
+		description: "djeiorudfsdfswiouerwfdfsdfieuriweuriu",
+	},
+];
+
+module.exports = { people };
