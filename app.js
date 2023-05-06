66e03a4291da7718e9059d04ec050f537f1475c1
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-06 04:20:32 +0000

    update express.js

--- app.js
+++ app.js
@@ -1,33 +1,11 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-
-app.use(express.static(path.resolve(__dirname, "./public")));
+const { people } = require("./EXPRESS/data");
 
 app.get("/", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "./EXPRESS/index.html"));
-});
-
-app.get("/about", (req, res) => {
-	res.status(200).send("This is the about page");
-});
-
-app.get("*", (req, res) => {
-	res.status(404).send("The resource was not found");
+	res.status(200).json({ success: true, data: people });
 });
 
 app.listen(5000, () => {
-	console.log("running on port 5000");
+	console.log("server is listening on port 5000...");
 });
-
-// const server = app.createServer((req,res)=> {
-
-// })
-
-//app.get
-//app.post
-//app.put
-//app.delete
-//app.all
-//app.use
-//app.listen
